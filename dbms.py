73d0bcc97828664ef2e946993050bc79020c543a
Mihir Sahu <2002mihir@gmail.com>
2022-03-19 12:36:37 -0500

    Removed discordTag

--- dbms.py
+++ dbms.py
@@ -4,14 +4,14 @@ import sqlite3
 def connect():
     conn = sqlite3.connect("databases/user.db")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS user (id INT PRIMARY KEY, discordTag VARCHAR(20), discordId VARCHAR(50), happy INT, sad INT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS user (id INT PRIMARY KEY, discordId VARCHAR(50), happy INT, sad INT)")
     conn.commit()
     conn.close()
 
-def insert(discordTag, discordId, happy, sad):
+def insert(discordId, happy, sad):
     conn = sqlite3.connect("databases/user.db")
     cur = conn.cursor()
-    cur.execute("INSERT INTO user VALUES(NULL,?,?,?,?)",(discordTag, discordId, happy, sad))
+    cur.execute("INSERT INTO user VALUES(NULL,?,?,?)",(discordId, happy, sad))
     conn.commit()
     conn.close()
 
@@ -23,10 +23,10 @@ def view():
     conn.close()
     return rows
 
-def search(discordTag="", discordId=""): #Only one section should be able to be inputted, so to avoid error set default values to ""
+def search(discordId=""): #Only one section should be able to be inputted, so to avoid error set default values to ""
     conn = sqlite3.connect("databases/user.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM user WHERE discordTag=? OR discordId=?",(discordTag, discordId)) #OR so that only one section can be inputted to get values
+    cur.execute("SELECT * FROM user WHERE discordId=?",(discordId)) #OR so that only one section can be inputted to get values
     rows = cur.fetchall()
     conn.close()
     return rows
@@ -38,9 +38,9 @@ def delete(discordId):
     conn.commit()
     conn.close()
 
-def update(discordTag, discordId, happy, sad):
+def update(discordId, happy, sad):
     conn = sqlite3.connect("databases/user.db")
     cur = conn.cursor()
-    cur.execute("UPDATE user SET discordTag=?,happy=?,sad=? WHERE discordId=?",(discordTag, happy, sad, discordId))
+    cur.execute("UPDATE user SET happy=?,sad=? WHERE discordId=?",(happy, sad, discordId))
     conn.commit()
     conn.close()
